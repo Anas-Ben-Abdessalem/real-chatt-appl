4239dd1e0dcad6c9cbc3f4dedfd5b784ed7c377c
Anas Ben Abdessalem <proffessionalanas1@gmail.com>
2022-12-20 19:42:32 +0100

    completed full profile image feature

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const User = require('./models/user');
 const session = require('express-session');
 const flash = require('connect-flash');
 const bcrypt = require('bcrypt');
+const multer = require('multer');
+const methodOverride = require('method-override');
+const fs = require("fs");
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/messageApp',
@@ -19,10 +22,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/messageApp',
     });
 
 
-
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 app.use(session({resave:true, secret: 'notagoodsecret', saveUninitialized:true }))
 app.use(flash());
 app.use((req,res,next) => {
@@ -31,11 +34,12 @@ app.use((req,res,next) => {
 })
 
 // MAIN CODE
- app.get('/', (req,res) => {
+ app.get('/', async(req,res) => {
     if (!req.session.user_id){
         res.redirect('/login');
     }else {
-        res.redirect('/secret');
+        let id = await bcrypt.hash(String(req.session.user_id),12)
+        res.redirect(`/users/${id.replaceAll('/','')}`);
     }
 });
 
@@ -51,7 +55,9 @@ app.post('/login', async (req,res) => {
         
         if(valid){
             req.session.user_id = user._id;
-            res.redirect('/secret');
+            let id = await bcrypt.hash(String(user._id),12);
+            id = id.replaceAll('/','');
+            res.redirect(`/users/${id }`);
 
         }
     }else{
@@ -75,12 +81,13 @@ app.post('/create', async (req,res) => {
         const user = new User({name, lastName, birthDate, email, password, registerDate:new Date()});
         await user.save();
         req.session.user_id = user._id;
-        res.redirect('/secret');
+        let id = await bcrypt.hash(String(user._id),12);
+        res.redirect(`/users/${ id.replaceAll('/','')}`);
     }
 
 });
 
-app.get('/secret', async (req,res) => {
+app.get('/users/:id', async (req,res) => {
     if(req.session.user_id){
         let user =  await User.findOne({'_id':req.session.user_id});
         const { name, lastName, birthDate, profile, ...others } = user;
@@ -102,12 +109,80 @@ app.get('/secret', async (req,res) => {
         let dbL = result[0].totalUsers[0].email
 
         result = result[0].data;
-        res.render('userPage',{ name, lastName, birthDate, profile, result,dbL})
+        let id = await bcrypt.hash(String(user._id),12);
+        id = id.replaceAll('/','');
+        res.render('userPage',{ name, lastName, birthDate, profile, result,dbL, includeEdit:false,id })
     }else{
         res.redirect('/')
     }
 });
 
+app.patch('/users/:id', async function(req, res) {
+    
+    let user =  await User.findOne({'_id':req.session.user_id});
+    
+    
+    const Storage = multer.diskStorage({
+        destination: function(req, file, callback) {
+            callback(null, "./public/media/uploads");
+        },
+        filename:async function(req, file, callback) {
+            callback(null, file.originalname);
+            if (file.originalname != ""){
+                const path = "./public/media/uploads/" + user.profile;
+
+                
+                    fs.unlink(path, function (err) {
+                        return 0
+                    });
+                    user.profile = file.originalname;
+                    await user.save()
+              
+                
+                }
+            }
+    });
+
+    const upload = multer({
+        storage: Storage
+    }).array("imgUploader",1); //Field name and max count
+    upload(req, res, function(err) {
+        if (err) {
+            return res.end("Something went wrong!");
+        }
+        return res.end("File uploaded sucessfully!");
+    });
+
+   
+    const {id} = req.params
+    res.redirect(`/users/${id}`);
+});
+
+app.get('/users/:id/edit', async(req, res) => {
+    let user =  await User.findOne({'_id':req.session.user_id});
+    const { name, lastName, birthDate, profile, ...others } = user;
+   //rendering 10 PEOPLE
+
+    let result = await User.aggregate([
+        {
+            $sort:{
+                registerDate: -1
+            }
+        },
+        {
+            $facet: {
+                totalUsers: [{$count: "email"}],
+                data:[{ $skip: 0 }, { $limit: 15 }]
+            }
+        }
+    ]);
+    let dbL = result[0].totalUsers[0].email
+
+    result = result[0].data;
+    let id = await bcrypt.hash(String(user._id),12);
+    id = id.replaceAll('/','');
+    res.render('userPage',{ name, lastName, birthDate, profile, result,dbL, includeEdit:true,id })
+});
 
 app.get('/moreUsers', async (req,res) => {
     
